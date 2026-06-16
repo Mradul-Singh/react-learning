68e7bcd888097a395f87ad017428c20630efd112
MRADUL SINGH <smradul977@gmail.com>
2026-06-16 11:23:20 +0530

    Built first React props and components project

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ProductTab from "./ProductTab.jsx";
+
+
+function App() {
+  return(
+  <>
+  <h2 style={{textAlign:"center"}}>Blockbuster Deals | Shop now</h2>
+  <ProductTab></ProductTab>
+  </>
+  
+  );
+}
+
+export default App
